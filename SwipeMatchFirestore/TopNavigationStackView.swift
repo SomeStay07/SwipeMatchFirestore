529ca54e375e977c87141667514b24b9ee3c7602
Timur Cheberda <likedune@gmail.com>
2019-04-08 21:17:02 +0300

    #2
    + Карточка и ее перемещение

--- SwipeMatchFirestore/TopNavigationStackView.swift
+++ SwipeMatchFirestore/TopNavigationStackView.swift
@@ -28,7 +28,7 @@ class TopNavigationStackView: UIStackView {
         
         distribution = .equalCentering
         isLayoutMarginsRelativeArrangement = true
-        layoutMargins = .init(top: 0, left: 12, bottom: 0, right: 12)
+        layoutMargins = .init(top: 0, left: 16, bottom: 0, right: 16)
     }
     
     required init(coder: NSCoder) {
