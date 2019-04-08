470c54209ca9a106327d802ccdc70e293b92be2f
Timur Cheberda <likedune@gmail.com>
2019-04-08 10:48:42 +0300

    Initial Commit

---
+++ SwipeMatchFirestore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeMatchFirestore
+//
+//  Created by Тимур Чеберда on 08/04/2019.
+//  Copyright © 2019 Tmur Cheberda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
